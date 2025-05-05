e43fe54b742efa8000ef0e5899f604044d6de2ae
M4slenits4 <heuberger.jonas@gmx.de>
2025-05-05 19:06:13 +0200

    Adjusted code for version 1

--- src/main/java/worker/Main.java
+++ src/main/java/worker/Main.java
@@ -4,28 +4,43 @@ import superdupermarkt.Cheese;
 import superdupermarkt.Product;
 import superdupermarkt.Wine;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
         List<Product> productList = new ArrayList<>();
-        productList.add(new Cheese("Cheddar", 30, new Date()));
-        productList.add(new Cheese("Tilsiter", 30, new Date()));
-        productList.add(new Cheese("Emmentaler", 30, new Date()));
+        productList.add(new Cheese("Cheddar", 90, Instant.now()));
+        productList.add(new Cheese("Tilsiter", 240, Instant.now()));
+        productList.add(new Cheese("Emmentaler", 190, Instant.now()));
 
-        productList.add(new Wine("Gutedel", 30, new Date()));
-        productList.add(new Wine("Chardonney", 30, new Date()));
-        productList.add(new Wine("Spätburgunder", 30, new Date()));
+        productList.add(new Wine("Gutedel", 30, Instant.now()));
+        productList.add(new Wine("Chardonney", 30, Instant.now()));
+        productList.add(new Wine("Spätburgunder", 30, Instant.now()));
 
-        for (int tage = 0; tage < 150; tage++) {
+        Instant startTime = Instant.now();
 
-            System.out.println(tage);
-            productList.forEach(e -> {
-                System.out.println(e.getLabel());
-            });
+        System.out.println("Startwerte des Programmes");
+        productList.forEach(Product::getProductDetails);
+
+        for (int anzahlTage = 0; anzahlTage < 150; anzahlTage++) {
+
+            System.out.println("Tag: " + (anzahlTage + 1 ) + " Datum: " + startTime);
+            System.out.println(" ");
+            for (Product product : productList) {
+                product.updateDailyPrice();
+                product.updateQualityLevel(startTime);
+                product.handleExpiration(startTime);
+                product.getProductDetails();
+                if (!product.checkQualityLevel()) {
+                    System.out.println("Produkt " + product.getLabel() + " muss entsorgt werden !");
+                }
+            }
+            startTime = startTime.plus(Duration.ofDays(1));
+            System.out.println("-------------------------------------------------------");
         }
     }
 }
