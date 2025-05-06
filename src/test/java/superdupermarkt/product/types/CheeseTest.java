7f32233659cfedf11c8bd15c21de5683d0863f7a
M4slenits4 <heuberger.jonas@gmx.de>
2025-05-06 21:11:37 +0200

    Added base structure for JUnit tests

---
+++ src/test/java/superdupermarkt/product/types/CheeseTest.java
@@ -0,0 +1,54 @@
+package superdupermarkt.product.types;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class CheeseTest {
+
+    @BeforeEach
+    public void setUp() {
+
+    }
+
+    @Test
+    public void checkQualityLevel_qualityAboveMinimum_returnsTrue() {
+
+    }
+
+    @Test
+    public void checkQualityLevel_qualityAtMinimum_returnsTrue() {
+
+    }
+
+    @Test
+    public void checkQualityLevel_qualityBelowMinimum_returnsFalse() {
+
+    }
+
+    @Test
+    public void updateQualityLevel_decreasesQualityDaily() {
+
+    }
+
+    @Test
+    public void updatePrice_updatesPriceBasedOnQuality() {
+
+    }
+
+    @Test
+    public void isExpired_expireDateInFuture_returnsFalse() {
+
+    }
+    @Test
+    public void isExpired_expireDateNow_returnsFalse() {
+
+    }
+    @Test
+    public void isExpired_expireDateInPast_returnsTrue() {
+
+    }
+
+}
