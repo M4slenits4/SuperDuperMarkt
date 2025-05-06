84ecc310272a3453dee48a9112b0f292ab532dee
M4slenits4 <heuberger.jonas@gmx.de>
2025-05-06 20:38:18 +0200

    updated java docs for all products and adjusted some methods

--- src/main/java/superdupermarkt/product/types/Product.java
+++ src/main/java/superdupermarkt/product/types/Product.java
@@ -1,4 +1,4 @@
-package superdupermarkt;
+package superdupermarkt.product.types;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,7 +9,8 @@ import java.math.RoundingMode;
 import java.time.Instant;
 
 /**
- * Define the abilities of the product
+ * The abstract class {@code Product} defines the basic properties and capabilities
+ * of every product in the supermarket. It serves as a base class for more specific product types. *
  */
 @AllArgsConstructor
 @Getter
@@ -28,7 +29,7 @@ public abstract class Product {
      * Calculate and round the daily price of a product.
      *
      * @param basicPrice    basic price of the product.
-     * @param quality       the quality of the product
+     * @param quality       the quality of the product.
      */
     public void calculatePrice(double basicPrice, int quality) {
         BigDecimal bigDecimal = new BigDecimal(Double.toString(basicPrice + (0.10 * quality)));
@@ -39,8 +40,8 @@ public abstract class Product {
     /**
      * Get out all needed product details of the product.
      */
-    public void getProductDetails() {
-        System.out.println("Bezeichnung: " + getLabel() + ", Preis: " + getPrice() + ", Qualität: " +  getQuality());
+    public String getProductDetails() {
+        return "Bezeichnung: " + getLabel() + ", Preis: " + getPrice() + ", Qualität: " + getQuality();
     }
 
     /**
@@ -52,16 +53,21 @@ public abstract class Product {
 
     /**
      * Update the quality level for the product. Depends on the product.
+     *
+     * @param toDay The current date and time.
      */
-    public abstract void updateQualityLevel(Instant today);
+    public abstract void updateQualityLevel(Instant toDay);
 
     /**
-     * Update daily price if it is changing for this product
+     * Update the price if there are changes  for the product.
      */
-    public abstract void updateDailyPrice();
+    public abstract void updatePrice();
 
     /**
-     *  Check for the expiry date and expire conditions of the product
+     * Checks for the expiry date of the product
+     *
+     * @param toDay The current date and time.
+     * @return {@code true} if the product is expired, otherwise {@code false}
      */
-    public abstract void handleExpiration(Instant today);
+    public abstract boolean isExpired(Instant toDay);
 }
